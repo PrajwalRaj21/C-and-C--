efba44bd40a1001501d214e6f480c631594bea46
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-21 19:16:44 +0545

    basic shape

---
+++ C/triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    printf("1\n");
+    printf(" 1\n");
+    printf("   1\n");
+    printf("     1\n");
+    printf("       1\n");
+    printf("         1\n");
+
+
+    return 0;
+
+}
