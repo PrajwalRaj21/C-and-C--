d9d3df4d2d068ecafcff30f69e5f7a8532b3e59f
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-21 19:16:37 +0545

    DataTypes and its defination

---
+++ C/DataTypes.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+    int age = 40;  //int datatype is used for whole number(for eg: 1,2,100)
+    double rank = 4.5; //double or float is normally used to denote a decimal number
+    char grade = 'A'; //char is used to denote a single character
+    char phrase[] = "Hello how are you?"; //string of text can be done here
+
+    //this C file contains basic datatpyes
+
+
+
+
+
+    return 0;
+}
