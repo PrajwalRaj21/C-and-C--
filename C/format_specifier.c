2ed5acc14d6849f442df40126377dba1c81ada5b
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-23 10:38:36 +0545

    format specifier

---
+++ C/format_specifier.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+
+int main(){
+
+    int favNum = 7;
+
+    printf("%d", 5400); 
+    printf("My favorite %s is %d", "number", 500); 
+    printf("My favorite %s is %f", "number", 500.66);
+    printf("My favorite %s is %d", "number", favNum);
+
+
+    /*%s prints string
+    %f prints float number
+    %d prints integar
+    %c prints single character*/
+    
+
+    return 0;
+}
