87397e31dadabb71f7d180f7f17431600648eb85
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-23 10:38:46 +0545

    working with numbers

---
+++ C/numbers.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+    //with variable
+
+    int num = 10;
+
+    printf("%d\n", num);
+
+
+//second part
+    printf("%f\n", 0.9);
+    printf("%f", 4.5 + 1.5);
+
+    //using complex mathematics functions
+
+   // printf("%d", pow(3,3));  //powers 3 to 3
+   // print("%f", sqrt(36));  //square root of 36
+
+
+
+    return 0;
+}
+
+
