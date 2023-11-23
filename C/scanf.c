c0ea2645aee0e986c40fa7ca4a602df5f4ea58e4
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-23 10:39:08 +0545

    commit

---
+++ C/scanf.c
@@ -0,0 +1,20 @@
+//scanf is used to get user input
+
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+
+    int age;
+
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("You are %d years old", age);
+    return 0;
+
+    
+
+
+
+}
