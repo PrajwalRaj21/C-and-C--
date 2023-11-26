ab7cb349684a376322998baf9f2ccd6d8836bd1c
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-26 15:05:05 +0545

    functions

---
+++ C/functions.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+//functions is basically a collection of code
+
+int main(){  
+
+
+
+    sayhi("Mike");
+    sayhi("Harry");
+    sayhi("Kristina");
+    sayhi("James");
+
+    
+    return 0;
+
+}
+
+void sayhi(char name[])
+{
+    printf("Hello %s\n", name);
+
+
+
+
+}                                 
