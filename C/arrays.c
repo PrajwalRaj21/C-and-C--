ce3496df12f1b164cc3d168276460d89338ec628
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-26 15:04:59 +0545

    learning arrays

---
+++ C/arrays.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int numbers[] = {1,2,3,7,6};
+
+    printf("%d", numbers[2]);  //prints out 3
+    printf("%d", numbers);
+    printf("%d", numbers[4]); //prints out 7
+
+
+
+
+
+
+    return 0;
+
+}
