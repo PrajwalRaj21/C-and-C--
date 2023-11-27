a5277dc760c166afc27c12bc4c19ff71f974ed5b
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-27 20:59:00 +0545

    Lab Report 1

---
+++ C/Workshop/Lab Report/Q_no_2.c
@@ -0,0 +1,23 @@
+//program to print the sum of digits of the number provided by user
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+    int num1, num2;
+
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    printf("The sum of the above given numbers are: %d", num1+num2);
+
+    return 0;
+
+
+    
+};
---
+++ C/Workshop/Lab Report/Q_no_3.c
@@ -0,0 +1,62 @@
+//calculator using swtich case
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int calculate(int num1, int num2, char operator){
+int result;
+    
+
+    switch(operator){
+        case '+':
+        result = num1 + num2;
+        break;
+
+        case '-':
+        result = num1 - num2;
+        break;
+
+
+        case '*':
+         result = num1 * num2;
+         break;
+
+        case '/':
+         result = num1 / num2;
+         break;
+
+
+         default:
+         printf("Invalid!");
+         return 0;
+
+
+    }
+
+    return result;
+};
+
+int main(){
+    int num1, num2;
+   char operator;
+
+   printf("Enter the first number: ");
+   scanf("%d", &num1);
+
+   printf("Enter the second number: ");
+   scanf("%d", &num2);
+
+   printf("Enter the operator (+, -, *, /): ");
+   scanf(" %c", &operator);
+
+   int result = calculate(num1, num2, operator);
+
+   if (result != 0) {
+      printf("The result is: %d", result);
+   }
+
+   return 0;
+};
+
