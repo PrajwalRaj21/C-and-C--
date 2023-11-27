c6afa22c6183e8d5c82bb34c4b82ac46cc231d3b
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-27 20:58:34 +0545

    If statement

---
+++ C/if.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int max(int num1, int num2){
+    if (num1 < num2)
+    {
+        printf("Great")
+
+    } else{
+        printf("OOPS!");
+    }
+    return result;
+
+}
+
+int main(){
+    printf("%d", max(4, 10));
+
+    return 0;
+}
