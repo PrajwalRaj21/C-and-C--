f78cafb092a0746ab95da8e95a06a36e502575f9
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-29 19:38:06 +0545

    Updated

--- C/Workshop/Lab Report/Q_no_2.c
+++ C/Workshop/Lab Report/Q_no_2.c
@@ -1,26 +1,27 @@
-//program to print the sum of digits of the number provided by user
+#include <stdio.h>
 
-#include<stdio.h>
-#include<stdlib.h>
+// Function to calculate the sum of digits of a number
+int calculateSum(int num) {
+    int sum = 0;
+    while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
 
+int main() {
+    int n, num, sum;
 
-int main(){
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
 
-    int num1, num2;
-
-
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-
-    printf("The sum of the above given numbers is: %d", num1+num2);
+    for (int i = 0; i < n; i++) {
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        sum = calculateSum(num);
+        printf("Sum of digits of %d is %d\n", num, sum);
+    }
 
     return 0;
-
-
-    
-};
-
-
+}
