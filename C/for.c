48e32973dc5ac92e73b03e5f6bde3700896db112
PrajwalRaj21 <christprajwal@gmail.com>
2023-11-29 11:20:04 +0545

    for loop

---
+++ C/for.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+ int main(){
+
+   int Numbers[] = {1,2,3,4,5,6};
+   int i;
+
+    for(i=0; i<=6; i++){
+        printf("%d\n", Numbers[i]);
+        
+    }
+
+    return 0;
+ }
