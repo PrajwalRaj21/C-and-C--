19ce5344d72ff9ed0f8dcc1853b48bfa4cce58da
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-08 19:33:03 +0545

    Initial commit

---
+++ C++/DataTypes.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    char grade = 'A';      //char stores a single data like A, B, C, D
+    string studentName = "Dolphin Academy";   //string a long string
+    int age = 50;  //while storing number we don't need quotation mark
+   // float = "2.00";
+   // double = "2.0000000";
+   // bool isMale = True; //boolean stores true or false
+
+    cout<<"The grade is "<<grade<<endl;
+    cout<<"The academy name is "<<studentName<<endl; 
+
+    return 0;
+ 
+
+
+}
---
+++ C++/variables.cpp
@@ -0,0 +1,20 @@
+// variables in C++
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    age = 21;
+
+    
+    string name = "Prajwal";
+
+
+cout <<"There once was a man named "<<name<< endl;
+cout << "He was "<<age<<" years old" << endl;
+cout <<"He liked the name "<<name<< endl;
+cout << "But did not like being "<<age <<endl;
+
+return 0;
+}
