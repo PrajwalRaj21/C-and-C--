8072c919d6b0d87d9f251b83999f89b6ead707dc
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-09 16:40:55 +0545

    working with numbers

---
+++ C++/numbers.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+
+int main(){
+
+    int wnum = 5;
+    double dnum = 5.5555;
+
+    wnum++;
+    wnum += 80;
+
+    cout<<"The whole number is: "<<wnum<<endl;
+
+    cout<< 5 + 5.555<<endl;
+
+
+    cout<< 100;
+    cout<<-40.444<<endl;
+    cout<<5+7<<endl;
+    cout<<5-7<<endl;
+
+    cout<<5*7<<endl;
+    cout<<5/7<<endl;
+
+    cout<<10%3<<endl; //% represent modulus 
+
+
+    cout<<pow(2, 5)<<endl; //prints out 32
+    cout<<sqrt(36); //prints out 6
+    
+
+
+
+
+
+
+
+    return 0;
+}
