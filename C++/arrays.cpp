2b9ae7622432b2969b543350c56b266f0c0b066c
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-10 20:56:50 +0545

    arrays in C++

---
+++ C++/arrays.cpp
@@ -0,0 +1,20 @@
+//arrays in C++
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int luckyNumbers[5]={1,2,3,4,5};
+    
+
+
+    cout<< "Lucky numbers are "<<luckyNumbers<<endl;
+    cout<<luckyNumbers[4];
+
+
+    return 0;
+
+
+    
+} 
+
