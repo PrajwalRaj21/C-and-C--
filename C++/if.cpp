f6c1e78d88f5f20189f0b490f10ead27bf62d617
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-11 10:07:36 +0545

    IF statement

---
+++ C++/if.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    bool isMale = false;
+    if(isMale){
+        cout<<"Hello, You are a male";
+    }else{
+        cout<<"Hello, You are a female?maybe?";
+    }
+
+}
