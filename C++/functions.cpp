554f753dd987b1de1dbced2213f734ec479db8fa
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-11 10:07:30 +0545

    using functions

---
+++ C++/functions.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void sayHi(string name, int age){
+    cout<<"Hello "<<name<<"You are "<<age<<endl;
+}
+
+int main(){  
+
+    sayHi("Jerry", 60);
+    sayHi("Tom", 60);
+    sayHi("Steve", 60);
+    sayHi("kat", 60);
+
+    return 0;
+
+                                     
+
+
+}
