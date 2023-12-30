43a894a6aec1b988929449671685723c82358cb1
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-30 21:03:25 +0545

    for loop

---
+++ C++/for_loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    for(int i=1; i<10; i++)
+    {
+        cout<<"This is a for loop"<<endl;
+        cout<<i<<endl;
+        
+
+    }
+    return 0;
+
+}
