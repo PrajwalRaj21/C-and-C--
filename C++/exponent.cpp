b62171b4294c79710e94dcb0cc8251287f88883e
PrajwalRaj21 <christprajwal@gmail.com>
2023-12-30 21:03:15 +0545

    finding power using functions

---
+++ C++/exponent.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int power(double baseNum, int powNum){
+    int result;
+    return result;
+    for(int i=0; i<powNum; i++)
+    {
+        result = result * baseNum;
+
+    }
+
+}
+
+int main(){
+
+    cout<<power(4,3);
+
+
+    return 0;
+    
+}
